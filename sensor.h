f548d2aefa46a2919ac769efccc28c76a3a02e83
Hugo Carola <hugocarola@ua.pt>
2024-04-02 10:56:32 +0100

    circular buffer update

--- sensor.h
+++ sensor.h
@@ -10,6 +10,7 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 #define MAX_STRING_SIZE 16 /**< Maximum number of elements in data string */
+#define BUFFER_SIZE 20
 #define FRAME_START '#'
 #define FRAME_END '!'
 
@@ -26,6 +27,67 @@ typedef enum {
     CMD_AIRQ,
 }command_t;
 
+typedef struct {
+    uint8_t * const temp_cbuffer;
+    int head;
+    int tail;
+    uint16_t maxlen;
+    uint16_t count;
+} circ_bbuf_T;
+
+/**
+ * @brief Inserts a new element into the DLL.
+ * @param dll Pointer to the DLL structure.
+ * @param key Key of the new element.
+ * @param data Data of the new element.
+ * @return 0 on success, -1 on failure.
+ */
+
+void Data_buff_Init(circ_bbuf_T *circ_buff, uint16_t max_elements)
+{
+    circ_buff->head = NULL;
+    circ_buff->tail = NULL;
+    circ_buff->maxlen = max_elements;
+    circ_buff->count = 0;
+}
+
+
+int Data_buff_Insert(circ_bbuf_T *circ_buff, unsigned char *data)
+{
+    int next;
+
+    next = circ_buff->head + 1;  // next is where head will point to after this write.
+    if (next >= circ_buff->maxlen)
+        next = 0;
+
+    if (next == circ_buff->tail)  // if the head + 1 == tail, circular buffer is full
+        return -1;
+
+    circ_buff->temp_cbuffer[circ_buff->head] = data;  // Load data and then move
+    circ_buff->head = next;             // head to next data offset.
+    return 0;  // return success to indicate successful push.
+}
+
+typedef struct {
+    char data[BUFFER_SIZE];
+    int front;
+    int rear;
+} Rx_Buffer;
+
+int reset(Rx_Buffer *buffer) {
+    int i = buffer->front;
+    // Procura pelo próximo EOF (end of frame)
+    while (buffer->data[i] != '!') {
+        i = (i + 1) % BUFFER_SIZE;
+        // Se voltar para a posição inicial, significa que o EOF não foi encontrado
+        if (i == buffer->front) {
+            printf("Erro: EOF não encontrado no buffer.\n");
+            return;
+        }
+    }
+    // Define o front para a próxima posição após o EOF
+    return buffer->front = (i + 1) % BUFFER_SIZE;
+}
 
 typedef struct 
 {
